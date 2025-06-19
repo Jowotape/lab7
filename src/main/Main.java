a2b36825bda44a5e69dfcf4f7dfb7ec9c870336f
LuisCal <luis.calderon2@utp.ac.pa>
2025-06-19 09:27:13 -0500

    hoochie mama

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,11 +2,18 @@ package main;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import prob1.*;
+import prob2.*;
+import prob3.*;
+import prob4.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Prob1 prob1 = new Prob1();
+
+        prob1.prob
     }
 }
--- src/prob1/Prob1.java
+++ src/prob1/Prob1.java
@@ -1,4 +1,37 @@
 package prob1;
 
 public class Prob1 {
+    private int filas;
+    private int columnas;
+    pritvate int[][] matriz;
+
+    public Prob1() {
+        this.filas = 0;
+        this.columnas = 0;
+    }
+
+    public int getFilas() {
+        return filas;
+    }
+    public int getColumnas() {
+        return columnas;
+    }
+    public int[][] getMatriz() {
+        return matriz;
+    }
+
+    public void setFilas(int filas) {
+        this.filas = filas;
+    }
+    public void setColumnas(int columnas) {
+        this.columnas = columnas;
+    }
+    public void setMatriz() {
+        this.matriz = new int[filas][columnas];
+    }
+
+    public void problemarse2(BufferedReader br) throws IOException {
+        setColumnas(br.readLine);
+    }
 }
+

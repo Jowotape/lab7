a61772071cbe87d03b5457eac86480f006ea3ea4
Jowotape <89794030+Jowotape@users.noreply.github.com>
2025-06-19 08:34:14 -0500

    Goro Akechi se divorcia y se lleva los niños

---
+++ lab 7/src/exceptions/EjemploException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class EjemploException extends RuntimeException {
+    public EjemploException(String message) {
+        super(message);
+    }
+}
+
+//Cambien esto a lo que necesiten y luego agreguen otras excepciones si necesario

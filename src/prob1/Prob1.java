fcc5143dbdc4c51204390810b18400c3975aaa87
gabriel_1607 <gabriel.cedeno3@utp.ac.pa>
2025-06-21 02:55:55 -0500

    problema1
    si hice alguna kgda digan con confianza, es la primera vez que uso esto

--- src/prob1/Prob1.java
+++ src/prob1/Prob1.java
@@ -6,7 +6,8 @@ import java.io.BufferedReader;
 public class Prob1 {
     private int filas;
     private int columnas;
-    private int[][] matriz;
+    private int[][] matrizOg;
+    private int[][] matrizTras;
 
     public Prob1() {
         this.filas = 0;
@@ -20,7 +21,11 @@ public class Prob1 {
         return columnas;
     }
     public int[][] getMatriz() {
-        return matriz;
+        return matrizOg;
+    }
+
+    public int[][] getMatrizTrans() {
+        return matrizTras;
     }
 
     public void setFilas(int filas) {
@@ -29,16 +34,65 @@ public class Prob1 {
     public void setColumnas(int columnas) {
         this.columnas = columnas;
     }
-    public void setMatriz() {
-        this.matriz = new int[filas][columnas];
+    public void setMatrizOg() {
+        this.matrizOg = new int[filas][columnas];
+    }
+
+    public void setMatrizTrans() {
+        this.matrizTras = new int[columnas][filas];
     }
 
     public void problemarse1(BufferedReader br) throws IOException {
         validarInput(br);
+        ingresarElementos(br);
+        calcularTraspuesta();
+        imprimirMatrices();
     }
 
     public void validarInput(BufferedReader br) throws IOException {
+        System.out.print("Ingrese el número de filas de la matriz: ");
+        this.setFilas(Integer.parseInt(br.readLine()));
 
+        System.out.print("Ingrese el número de columnas de la matriz: ");
+        this.setColumnas(Integer.parseInt(br.readLine()));
+
+        this.setMatrizOg();
+        this.setMatrizTrans();
     }
-}
 
+    public void ingresarElementos (BufferedReader br) throws IOException{
+        System.out.println("Ingrese los elementos de la matriz");
+        for (int i = 0; i < this.filas; i++){
+            for (int j = 0; j < this.columnas; j++){
+                System.out.print("Ingrese el elemento de la posicion [" + i + "][" + j + "] ");
+                this.matrizOg[i][j] = Integer.parseInt(br.readLine());
+            }
+        }
+    }
+
+    public void calcularTraspuesta() {
+        for (int i = 0; i < this.filas; i++) {
+            for (int j = 0; j < this.columnas; j++){
+                this.matrizTras[j][i] = this.matrizOg[i][j];
+            }
+        }
+    }
+
+    private void imprimirMatriz(int[][] matriz, int numFilas, int numColumnas) {
+        for (int i = 0; i < numFilas; i++) {
+            for (int j = 0; j < numColumnas; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public void imprimirMatrices() {
+        System.out.println("\nMatriz Original:");
+        imprimirMatriz(this.matrizOg, this.filas, this.columnas);
+
+        System.out.println("\nMatriz Traspuesta:");
+        imprimirMatriz(this.matrizTras, this.columnas, this.filas); // Las dimensiones son invertidas para la traspuesta
+    }
+
+}
